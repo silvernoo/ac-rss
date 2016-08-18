9fe5b21c0d4a741e5355c4e55660916c58495f50
uedeck <uedeck@gmail.com>
2016-08-18 17:14:56 +0800

    done

--- gen.sh
+++ gen.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
-python acfun_feed_110.py &&
-python acfun_feed_164.py &&
-python acfun_feed_73.py &&
-python acfun_feed_74.py &&
-python acfun_feed_75.py
+python generator/acfun_feed_110.py &&
+python generator/acfun_feed_164.py &&
+python generator/acfun_feed_73.py &&
+python generator/acfun_feed_74.py &&
+python generator/acfun_feed_75.py
